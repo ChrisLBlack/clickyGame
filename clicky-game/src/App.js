4856c0a464effd219403313935b8690d825c62c9
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-10-17 19:32:57 -0500

    fixed css, starting to work on logic

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import NavBar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Jumbo from "./components/JumboTron/Jumbo";
-// import Image from "./components/Image/Image";
+import Wrapper from "./components/Wrapper/Wrapper";
 import ImageCard from "./components/ImageCard/ImageCard";
 import images from "./images.json";
 import "./App.css";
@@ -16,13 +16,11 @@ class App extends Component {
       <div>
         <NavBar />
         <Jumbo />
-
-        <div>
+        <Wrapper>
           {this.state.images.map(pic => (
             <ImageCard key={pic.id} id={pic.id} image={pic.image} />
           ))}
-        </div>
-
+        </Wrapper>
         <Footer />
       </div>
     );
