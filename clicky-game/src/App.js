1d27d4d3033a2f718425b3f3a127cd8e9ccac2bd
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-10-17 19:21:45 -0500

    making progress on the images container

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import NavBar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Jumbo from "./components/JumboTron/Jumbo";
-import Image from "./components/Image/Image";
+// import Image from "./components/Image/Image";
+import ImageCard from "./components/ImageCard/ImageCard";
 import images from "./images.json";
 import "./App.css";
 
@@ -18,7 +19,7 @@ class App extends Component {
 
         <div>
           {this.state.images.map(pic => (
-            <Image key={pic.id} id={pic.id} image={pic.image} />
+            <ImageCard key={pic.id} id={pic.id} image={pic.image} />
           ))}
         </div>
 
